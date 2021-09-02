b4bc373a2f5925ba3c76bc9cf66d7875c325d1f9
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-09-01 21:03:49 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.mealCost = 0.00;
+            ViewBag.tip15 = 0;
+            ViewBag.tip20 = 0;
+            ViewBag.tip25 = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCost model)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.tip15 = model.CalcTip15(model.mealCost);
+                ViewBag.tip20 = model.CalcTip20(model.mealCost);
+                ViewBag.tip25 = model.CalcTip25(model.mealCost);
+            }
+            else
+            {
+                ViewBag.tip15 = 0;
+                ViewBag.tip20 = 0;
+                ViewBag.tip25 = 0;
+            }
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TipCost.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class TipCost
+    {
+        [Required(ErrorMessage = "You must enter a value for the cost of the meal")]
+        [Range(0, 1000000, ErrorMessage = "You must enter a value in between 0 & 1,000,000")]
+        public double? mealCost { get; set; }
+        public double? tip15 { get; set; }
+        public double? tip20 { get; set; }
+        public double? tip25 { get; set; }
+
+        public double? CalcTip15(double? mealCost)
+        {
+            double? total;
+
+            total = mealCost * .15;
+
+            tip15 = total;
+
+            return tip15;
+        }
+
+        public double? CalcTip20(double? mealCost)
+        {
+            double? total;
+
+            total = mealCost * .20;
+
+            tip20 = total;
+
+            return tip20;
+        }
+
+        public double? CalcTip25(double? mealCost)
+        {
+            double? total;
+
+            total = mealCost * .25;
+
+            tip25 = total;
+
+            return tip25;
+        }
+    }
+}
